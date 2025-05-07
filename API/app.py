fb6f11b48c1eb5d78abe0d7d64545f250b2f9b2f
JUAN ESTEBAN <cocoiral@hotmail.com>
2025-05-07 14:12:24 -0500

    super heroes marvel agregados

--- API/app.py
+++ API/app.py
@@ -14,3 +14,8 @@ def get_familia():
 def get_superheroes():
     rows = ["Superman", "Batman", "Flash", "Linterna Verde", "Mujer maravilla", "Aquaman", "Shazam", "Cyborg"]
     return rows
+
+@app.get("/superheroesMarvel")
+def get_superheroesM():
+    rows = ["ironman", "thor"]
+    return rows
