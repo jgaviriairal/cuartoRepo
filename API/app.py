36fb7f401c0737a84a13fbb4062f764a3b533039
JUAN ESTEBAN <cocoiral@hotmail.com>
2025-05-07 14:19:50 -0500

    cursos platzi agregados

--- API/app.py
+++ API/app.py
@@ -14,3 +14,9 @@ def get_familia():
 def get_superheroes():
     rows = ["Superman", "Batman", "Flash", "Linterna Verde", "Mujer maravilla", "Aquaman", "Shazam", "Cyborg"]
     return rows
+
+
+@app.get("/cursos")
+def get_cursos():
+    rows = ["python", "java", "sql"]
+    return rows
